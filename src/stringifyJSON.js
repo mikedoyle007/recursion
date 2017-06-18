da1f216064824a7fdf7d026b71054bcfb7c9f0f9
Mike Doyle <mikedoyle007@gmail.com>
2017-06-17 18:10:59 -0700

    Initial commit - some tests are passing
    Still need to make other tests pass, this is not the final
    version.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,42 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+	if (obj) {
+	  var str = '';
+	    if (Array.isArray(obj)) {
+	    	str = '[';
+	      if (obj.length) {
+	        for (var i = 0; i < obj.length; i++) {
+	        	str += stringifyJSON(obj[i]);
+	          if (i < (obj.length - 1)) {
+	          	str += ',';
+	          }
+	        }
+	      }
+	      str += ']';
+	    } else if (typeof(obj) === 'object') {
+	    	str = '{';
+	      var count = 0;
+	      var numOfProps = Object.keys(obj).length;
+	      
+	      for (var prop in obj) {
+	      	count++;
+	      	str += stringifyJSON(prop) + ':' + stringifyJSON(obj[prop]);
+	        if (count < numOfProps) {
+	          str += ',';
+	        }
+	      }
+	      str += '}';
+	    } else if (typeof(obj) === 'string') {
+	    	// str = '"' + obj + '"';
+	    	str = "'" + obj + "'";
+	    } else if (typeof(obj) === 'boolean') {
+	    	str = obj.toString();
+	    } else if (typeof(obj) === 'number') {
+	    	str = obj.toString();
+	    }
+	  } else {
+	  	str = 'null';
+	  }
+    return str;
 };
