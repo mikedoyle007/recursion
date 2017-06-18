14d6158fc2adb0e2297bc84e0ba6f8db4df59afe
Mike Doyle <mikedoyle007@gmail.com>
2017-06-17 19:43:54 -0700

    Pass all tests for stringifyJSON function
    Went back through and cleaned up indentation as well.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -6,47 +6,54 @@
 var stringifyJSON = function(obj) {
   console.log(obj);
   // your code goes here
-	if (obj !== null && obj !== undefined) {
-	  var str = '';
-	    if (Array.isArray(obj)) {
-	    	str = '[';
-	      if (obj.length) {
-	        for (var i = 0; i < obj.length; i++) {
-	        	str += stringifyJSON(obj[i]);
-	          if (i < (obj.length - 1)) {
-	          	str += ',';
-	          }
-	        }
-	      }
-	      str += ']';
-	    } else if (typeof(obj) === 'object') {
-	    	str = '{';
-	      var count = 0;
-	      var numOfProps = Object.keys(obj).length;
-	      
-	      for (var prop in obj) {
-	      	count++;
-	      	str += stringifyJSON(prop) + ':' + stringifyJSON(obj[prop]);
-	        if (count < numOfProps) {
-	          str += ',';
-	        }
-	      }
-	      str += '}';
-	    } else if (typeof(obj) === 'string') {
-	    	str = '"' + obj + '"';
-	    	// str = "'" + obj + "'";
-	    } else if (typeof(obj) === 'boolean') {
-	    	str = obj.toString();
-	    } else if (typeof(obj) === 'number') {
-	    	str = obj.toString();
-	    }
-	  } else {
-	  	if (obj === null) {
-	  	  str = 'null';
-	  	} else if (obj === undefined) {
-	  		// do something
-	  	}
-	  	
-	  }
-    return str;
+  if (obj !== null && obj !== undefined) {
+    var str = '';
+    if (Array.isArray(obj)) {
+      str = '[';
+      if (obj.length) {
+        for (var i = 0; i < obj.length; i++) {
+          str += stringifyJSON(obj[i]);
+          if (i < (obj.length - 1)) {
+            str += ',';
+          }
+        }
+      }
+      str += ']';
+    } else if (typeof(obj) === 'object') {
+      str = '{';
+      var count = 0;
+      var numOfProps = Object.keys(obj).length;
+      for (var prop in obj) {
+      	count++;
+      	if (typeof(obj[prop]) !== 'function' && typeof(obj[prop]) !== undefined) {
+      	  str += stringifyJSON(prop) + ':' + stringifyJSON(obj[prop]);
+      	  if (count < numOfProps) {
+      	    str += ',';
+      	  } 
+      	} else {
+          return '{}';
+      	}
+      }
+      str += '}';
+    } else if (typeof(obj) === 'string') {
+      if (obj === 'functions' || obj === 'undefined') {
+        str = '';
+      } else {
+        str = '"' + obj + '"';
+      }
+    } else if (typeof(obj) === 'boolean') {
+      str = obj.toString();
+    } else if (typeof(obj) === 'number') {
+      str = obj.toString();
+    } else if (typeof(obj) === 'function') {
+      str = '';
+    }
+  } else {
+    if (obj === null) {
+  	  str = 'null';
+  	} else if (obj === undefined) {
+  	  str = '';
+  	}
+  }
+  return str;
 };
