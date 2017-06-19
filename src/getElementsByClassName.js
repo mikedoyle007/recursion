7e23f6e6947d9ea9095f1241df2dec19437418bb
Mike Doyle <mikedoyle007@gmail.com>
2017-06-18 22:50:38 -0700

    Needs work - getElementByClassName function not working yet.
    I just started a different approach and wanted to save it, as I
    believe it has some promise.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,21 +6,58 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className) {
   // your code here
-  console.log(className);
-  console.log(document.body);
+
   var elements = [];
-    var body = document.body;
-    
-    if (body.hasChildNodes()) {
-    	var children = body.childNodes;
-  		for (var i = 0; i < children.length; i++) {
-      	var child = children[i];
-      	if (child.classList !== undefined) {
-        	if (child.classList.contains(className)) {
-        	  elements.push(child);
+  var body = document.body;
+  
+  var findChildElementsByClassName = function(element, className) {
+  	var childElements = [];
+    /*  MAY NEED TO MOVE THIS IF STATEMENT OUT OF THIS FUNCTION  */
+    if (element.hasChildNodes()) {
+      var children = element.childNodes;
+      // search the child nodes
+      for (var i = 0; i < children.length; i++) {
+        var child = children[i];
+        // ensure not undefined
+        if (child.classList !== undefined) {
+          // then look for the className
+          if (child.classList.contains(className)) {
+            // if found, add 
+            childElements.push(child);
+          } else if (child.hasChildNodes()) {
+            var children = findChildElementsByClassName(child, className);
+            if (children.length) {
+            	childElements.push(findChildElementsByClassName(child, className));
+            } 
           }
         }
       }
     }
-    return elements;
+    return childElements;
+  };
+  
+  elements = findChildElementsByClassName(body, className);
+  // need to flatten out before returning
+  _.flatten(elements);
+  return elements;
 };
+
+
+  // console.log(className);
+  // console.log(document.body);
+  // var elements = [];
+  //   var body = document.body;
+    
+  //   if (body.hasChildNodes()) {
+  //   	var children = body.childNodes;
+  // 		for (var i = 0; i < children.length; i++) {
+  //     	var child = children[i];
+  //     	if (child.classList !== undefined) {
+  //       	if (child.classList.contains(className)) {
+  //       	  elements.push(child);
+  //         }
+  //       }
+  //     }
+  //   }
+  //   return elements;
+
