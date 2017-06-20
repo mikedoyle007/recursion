8b853cf848852a1b30b649c9cc02bebf34945267
Mike Doyle <mikedoyle007@gmail.com>
2017-06-19 17:28:06 -0700

    Working version - getElementsByClassName function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,40 +5,35 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className) {
-  // your code here
-
   var elements = [];
   var body = document.body;
   
   var findChildElementsByClassName = function(element, className) {
-  	var childElements = [];
-    /*  MAY NEED TO MOVE THIS IF STATEMENT OUT OF THIS FUNCTION  */
-    if (element.hasChildNodes()) {
-      var children = element.childNodes;
-      // search the child nodes
-      for (var i = 0; i < children.length; i++) {
-        var child = children[i];
-        // ensure not undefined
-        if (child.classList !== undefined) {
-          // then look for the className
-          if (child.classList.contains(className)) {
-            // if found, add 
-            childElements.push(child);
-          } else if (child.hasChildNodes()) {
-            var children = findChildElementsByClassName(child, className);
-            if (children.length) {
-            	childElements.push(findChildElementsByClassName(child, className));
-            } 
-          }
-        }
+		var childElements;
+    
+    // does current element contain the class
+    if (element.classList !== undefined) {
+    	if (element.classList.contains(className)) {
+        childElements = element;
       }
     }
-    return childElements;
+    if (childElements !== undefined) {
+    	elements.push(childElements);
+    }
+    
+    
+    // look for the class via the children
+    if (element.hasChildNodes()) {
+      var children = element.childNodes;
+      for (var i = 0; i < children.length; i++) {
+        var child = children[i];
+        if (child.classList !== undefined) {
+        	findChildElementsByClassName(child, className);
+        }
+      } 
+    } 
   };
-  
-  elements = findChildElementsByClassName(body, className);
-  // need to flatten out before returning
-  _.flatten(elements);
+  findChildElementsByClassName(body, className);
   return elements;
 };
 
