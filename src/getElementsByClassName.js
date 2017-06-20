cbc3fc78805f52ed6662b2e574b24a0d348caef9
Mike Doyle <mikedoyle007@gmail.com>
2017-06-19 17:55:23 -0700

    Clean up code/refactor - getElementsByClassName
    I condensed the code by removing some comments and removing
    repetitive code.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -11,48 +11,24 @@ var getElementsByClassName = function(className) {
   var findChildElementsByClassName = function(element, className) {
 		var childElements;
     
-    // does current element contain the class
     if (element.classList !== undefined) {
-    	if (element.classList.contains(className)) {
-        childElements = element;
+    	if (element.classList.contains(className) && element !== undefined) {
+        elements.push(element);
       }
     }
-    if (childElements !== undefined) {
-    	elements.push(childElements);
-    }
     
-    
-    // look for the class via the children
     if (element.hasChildNodes()) {
       var children = element.childNodes;
       for (var i = 0; i < children.length; i++) {
         var child = children[i];
-        if (child.classList !== undefined) {
-        	findChildElementsByClassName(child, className);
-        }
+        findChildElementsByClassName(child, className);
       } 
     } 
   };
+
   findChildElementsByClassName(body, className);
   return elements;
 };
 
 
-  // console.log(className);
-  // console.log(document.body);
-  // var elements = [];
-  //   var body = document.body;
-    
-  //   if (body.hasChildNodes()) {
-  //   	var children = body.childNodes;
-  // 		for (var i = 0; i < children.length; i++) {
-  //     	var child = children[i];
-  //     	if (child.classList !== undefined) {
-  //       	if (child.classList.contains(className)) {
-  //       	  elements.push(child);
-  //         }
-  //       }
-  //     }
-  //   }
-  //   return elements;
 
